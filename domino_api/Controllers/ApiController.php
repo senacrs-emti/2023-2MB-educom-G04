41e4070ab3c12fa56a2e984038b6b681e078f1e8
Arthur-Fetzner <68615679+Arthur-Fetzner@users.noreply.github.com>
2023-10-31 11:29:47 -0300

    V1.0
    Fetzner

--- domino_api/Controllers/ApiController.php
+++ domino_api/Controllers/ApiController.php
@@ -10,13 +10,39 @@ class ApiController
         header('Content-Type: application/json');
     }
 
+    private function returnError($data, $message)
+    {
+        $data['description'] = $message;
+        die(json_encode($data));
+    }
+
     public function getAction()
     {
         // Aqui farei a vaiidação dos dados enviados e chamarei a função específica para trarar os dados
         // Essa função irá utilizar do model para recuperar os dados do banco de dados ou chamar as funções espeficidas de outras classes
-        $result = $this->model->getAllData();
         $data = array();
-        $data['error'] = false;
+        $data['error'] = true;
+        
+        if (!isset($_GET['command']))
+            $this->returnError($data, 'Command to execute was not send');
+
+        $command = $_GET['command'];
+
+        if ($command == "create_room")
+        {
+            if (!isset($_GET['room_name']))
+                $this->returnError($data, 'Room name was not send'); 
+
+            $data['error'] = false;
+            $data['room_name'] = $_GET['room_name']; // aqui chamar a função que realizará a logica e salvar no bd 
+            die(json_encode($data)); 
+        }
+        else
+            $this->returnError($data, 'Invalid command');
+
+        
+        /*
+        $result = $this->model->getAllData();
     
         if ($result === false)
         {
@@ -28,8 +54,9 @@ class ApiController
             {
                 while ($row = $result->fetch_assoc())
                     $data[] = $row;
-            }
+            }   
         }
+        */
 
         echo json_encode($data);
     }
